c8b51e805f57b1db29f107387fc2a1dab8e90d96
Marcus Vinicius Bianchi dos Santos <marcuss@spi.local>
2018-02-05 11:14:40 -0200

    Changes to cloud deploy

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace recipeservice {
 
         public static IWebHost BuildWebHost (string[] args) =>
             WebHost.CreateDefaultBuilder (args)
-            //.UseUrls("http://*:5002")
+            .UseUrls ("http://*:5002")
             .UseStartup<Startup> ()
             .Build ();
     }
